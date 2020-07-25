a72b19091e2b5e2d404d99dcc0f7e765539337bf
Md. Mehedi Khan <mehedidumis@gmail.com>
2020-07-25 21:43:06 +0600

    changes for next lecture

--- memory_management.py
+++ memory_management.py
@@ -1,5 +1,5 @@
 import sys
-
+print(f"Lecture 1: Variables and Reference counter")
 # memory management Deep Dive Python 3
 10  # 10 is an object with value 10
 a = 10  # 10 is an object , a refers to the memory location of 10
@@ -12,6 +12,9 @@ d = c  # 3rd
 id(d)  # after getting the address id function releases its pointer as a result it doesn't get counted
 # get ref count counts the reference of an object
 print(sys.getrefcount(d))  # This function refers the object 10 by using 'a' pointer too # 4th
+
+print(f"Lecture 2: Garbage Collection\n\n")
+
 
 
 
